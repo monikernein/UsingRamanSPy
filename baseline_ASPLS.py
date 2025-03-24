c9c60401f8bfd9f849098a6f4e523566510e5595
sarah <sarah@insidevault.com>
2025-03-24 11:50:51 +0100

    Separating one of the baseline commands into a single script

---
+++ baseline_ASPLS.py
@@ -0,0 +1,34 @@
+import argparse
+import ramanspy as rp
+
+parser = argparse.ArgumentParser()
+
+# Adding file path argument
+parser.add_argument("-f", "--File", help = "File to parse")
+
+# Read arguments from command line
+args =  parser.parse_args()
+
+if args.File:
+    print("File loaded is: %s " % args.File)
+    raman_spectrum =  rp.load.witec(args.File)
+else:
+    # Send an error
+    print ("File argument not specified with -f \"/path/to/file.mat\"")
+    exit()
+
+# Create baseline preprocessing pipeline, there are others that can be used
+# pipeline = rp.preprocessing.Pipeline([rp.preprocessing.baseline.ASPLS()])
+# Example with baseline, spike removal, denoising, cropping 
+pipeline = rp.preprocessing.Pipeline([
+    rp.preprocessing.baseline.ASPLS()
+])
+
+# Run the pipeline on the data
+data = pipeline.apply(raman_spectrum)
+
+# Make a popup happen that shows the original and the processed graphs
+# rp.plot is basically an image object so once you set things like spectra you'll get something out of show()
+rp.plot.spectra([raman_spectrum, data], label=["original","processed"], title='Useful Preprocessing Comparison', plot_type="stacked")
+rp.plot.show()
+
