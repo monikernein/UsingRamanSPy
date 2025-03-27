6f0a3537be3abf99bd24f428b992c341109f46c4
sarah <sarah@insidevault.com>
2025-03-27 22:28:37 +0100

    example mocking of two ramanspy calls in the unit test

--- baseline_ASPLS_test.py
+++ baseline_ASPLS_test.py
@@ -1,31 +1,30 @@
 import unittest
-from baseline_ASPLS import BaselineASPLS
+# import pytest
+import baseline_ASPLS
+# from baseline_ASPLS import BaselineASPLS
+from unittest.mock import patch
 
 class TestBaselineASPLS(unittest.TestCase):
 
-    #def setUp(self):
-    #    mode = 'unittest'
-    #    test_param = {
-    #        'file_name': './TestSpectra.mat'
-    #    }
-    #    self.processor = BaselineASPLS(mode, test_param)
-
-    def test_success(self):
+    @patch('ramanspy.load.witec')
+    @patch('ramanspy.preprocessing.Pipeline.apply')
+    def test_success(self, mock_load,  mock_pipeline_apply):
         test_cases = [
         ({'file_name': './TestSpectra.mat'}),
         ({'file_name': './TestImage.mat'})
         ]
         # This successfully calls the method we want to test
         # next step is to add mocking
-        # assert that loadfile is called
         # assert that create pipeline is called with correct parameter
         # assert that the image generator and show are called
         for test_param in test_cases:
-            with self.subTest(test_param =test_param):
-                self.processor = BaselineASPLS('unittest', test_param)
-                self.processor.preprocessFile()
-        # this makes the test always pass, delete after mocking is in
-                self.assertEqual('foo'.upper(), 'FOO')
+            thing = baseline_ASPLS.BaselineASPLS('unittest', test_param)
+            thing.preprocessFile()
+            # assert load file is called with file name
+            assert mock_load.called_with(test_param)
+            # assert pipeline apply is called
+            assert mock_pipeline_apply.called
+            # need to work a close window call into the show mock or the test will hang
 
 if __name__ == '__main__':
     unittest.main()
